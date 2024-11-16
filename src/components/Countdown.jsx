aaa80afb26dc25264ed1adacacd23e4daff2c981
tutich <pkoinange02@gmail.com>
2024-11-16 23:59:00 +0300

    project css, react components

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const countdown = () => {
+  return (
+    <div>
+        <section>
+            <p>We're launching soon Days Hours Minutes Seconds</p>
+        </section>
+    </div>
+  )
+}
+
+export default countdown
